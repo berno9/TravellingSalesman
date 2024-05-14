85134d8695445a58c9e4c96369a15180b22ff137
dimochii <diana.mm.nunes.28@gmail.com>
2024-05-14 08:47:07 +0100

    gtrfed

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ int main() {
     script.read_tourism();
     script.read_rwg_g1();
     TSPSolver tspSolver;
-    tspSolver.calculateTSP(script.getTmGraph());
-    tspSolver.calculateTriangleTSP(script.getTmGraph());
+    tspSolver.calculateTSP(script.getStGraph());
+    tspSolver.calculateTriangleTSP(script.getStGraph());
     //Menu menu = Menu(tspSolver, script);
     //menu.mainMenu();
 
